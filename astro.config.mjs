3414eee9aad4566ae8d7b163fa597f35ee7eaff2
body <abdo.mhamed122yahoo.com@gmail.com>
2025-02-05 05:19:59 +0200

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: vercel(),
+  output: "server",
+  adapter: netlify(),
 });
